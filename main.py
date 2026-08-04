b79955694daee3d9a9e51328b7935b08c4c32c05
nexorasistema <pazadelanexora@gmail.com>
2026-08-04 16:29:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from repl-nix-workspace!")
+from flask import Flask, render_template, request, jsonify
 
+# Crear la aplicación
+app = Flask(_name_)
 
-if __name__ == "__main__":
-    main()
+# Ruta de prueba principal
+@app.route('/')
+def inicio():
+    return "✅ ¡Sistema Nexora funcionando correctamente!"
+
+# Ejecutar la app en Render
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
