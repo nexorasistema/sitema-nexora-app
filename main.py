5ad8079e0001d3112c1e50813eaa9467da7388ca
nexorasistema <pazadelanexora@gmail.com>
2026-08-05 15:00:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-aplicacion = Flask(_name_)
+app = Flask(__name__)
 
-@aplicacion.route('/')
-def inicio():
-    return "✅ Sistema Nexora funcionando correctamente!"
+@app.route('/')
+def home():
+    return "Sistema Nexora funcionando correctamente!"
 
-if _name_ == "_main_":
-    aplicacion.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
   
