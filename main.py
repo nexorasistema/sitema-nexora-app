0f1ab1ee0a7d93c54c826368c8c5ec278e536e8a
nexorasistema <pazadelanexora@gmail.com>
2026-08-05 14:16:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 # Crear la aplicación
-app = Flask(_name_)
+aplicacion = Flask(__name__)
 
 # Ruta de prueba principal
-2am@app.route('/')
+@aplicacion.route('/')
 def inicio():
-    return "✅ Sistema Nexora funcionando correctamente!"
+    return "✅ ¡Sistema Nexora funcionando correctamente!"
 
-# Ejecutar la app en Render
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=5000)
+# Ejecutar la app
+if __name__ == "__main__":
+    aplicacion.run(host="0.0.0.0", port=5000)
